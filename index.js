cdaf72fa769f3f2d53ad65de28d087dfa8a666c2
Andrew Hsu <andrew.hsu@c02dl21umd6p.myfiosgateway.com>
2020-12-01 13:51:01 -0500

    added requireAuth to expenses and user routes

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get("/", async (req, res) => {
 
 // 👇 add requiresAuth middlware to these private routes  👇
 
-app.get("/user", async (req, res) => {
+app.get("/user", requireAuth(), async (req, res) => {
   res.render("user", {
     user: req.oidc && req.oidc.user,
     id_token: req.oidc && req.oidc.idToken,
@@ -81,7 +81,7 @@ app.get("/user", async (req, res) => {
   });
 });
 
-app.get("/expenses", async (req, res, next) => {
+app.get("/expenses", requireAuth(), async (req, res, next) => {
   res.render("expenses", {
     user: req.oidc && req.oidc.user,
     expenses,
